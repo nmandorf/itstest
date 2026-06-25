60d101e3b11ec47eabd98415e0f6b6eaca936a26
Noa Mandorf <noatomasmandorf@my.smccd.edu>
2026-06-25 14:22:43 -0700

    js renderd cards

---
+++ widgets.js
@@ -0,0 +1,77 @@
+export const widgets = [
+  {
+    title: "Student Email",
+    feature: true,
+    image: "images/Group 5.png",
+    imageAlt: "Student Email Logo",
+    description:
+      "All email sent from Cañada, CSM and Skyline Colleges goes to your free District provided email address. Part of Google Apps for Education.",
+    buttonText: "Sign In to Email",
+    buttonUrl: "http://mail.my.smccd.edu/"
+  },
+  {
+    title: "Canvas",
+    feature: true,
+    image: "images/Group 3.png",
+    imageAlt: "Canvas Logo",
+    description:
+      "Learn online courses and access your course materials remotely from a new online learning system.",
+    buttonText: "Login to Canvas",
+    buttonUrl: "http://smccd.instructure.com/"
+  },
+  {
+    title: "Websmart",
+    feature: true,
+    image: "images/Group 4.png",
+    imageAlt: "WebSmart Logo",
+    description:
+      "A web resource for viewing college data. Students can register for classes, download transcripts, apply for financial aid and much more!",
+    buttonText: "Visit Websmart",
+    buttonUrl: "https://websmart.smccd.edu"
+  },
+  {
+    title: "WebSchedule",
+    feature: false,
+    image: "https://smccd.edu/its-1/img/icons/Background.svg",
+    imageAlt: "WebSchedule",
+    description: "Search current and upcoming class schedules.",
+    buttonText: "Open WebSchedule",
+    buttonUrl: "#"
+  },
+  {
+    title: "AlertU",
+    feature: false,
+    image: "https://smccd.edu/its-1/img/icons/Background.svg",
+    imageAlt: "AlertU",
+    description: "Get emergency alerts and campus safety updates.",
+    buttonText: "Open AlertU",
+    buttonUrl: "#"
+  },
+  {
+    title: "Faculty Office Hours",
+    feature: false,
+    image: "https://smccd.edu/its-1/img/icons/Background.svg",
+    imageAlt: "Faculty Office Hours",
+    description: "Find instructor office hours and contact details.",
+    buttonText: "Open Faculty Office Hours",
+    buttonUrl: "#"
+  },
+  {
+    title: "Google Apps",
+    feature: false,
+    image: "https://smccd.edu/its-1/img/icons/Background.svg",
+    imageAlt: "Google Apps",
+    description: "Access District Google tools and apps.",
+    buttonText: "Open Google Apps",
+    buttonUrl: "#"
+  },
+  {
+    title: "Microsoft Office",
+    feature: false,
+    image: "https://smccd.edu/its-1/img/icons/Background.svg",
+    imageAlt: "Microsoft Office",
+    description: "Use Microsoft Office tools online.",
+    buttonText: "Open Microsoft Office",
+    buttonUrl: "#"
+  }
+];
